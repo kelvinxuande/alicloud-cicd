a17dcf2858513626e40d5c35dd6b91bee873f47b
kelvinxuande <37198547+kelvinxuande@users.noreply.github.com>
2025-04-13 10:34:18 +0800

    Update hello.tf

--- main/hello.tf
+++ main/hello.tf
@@ -1,3 +1,13 @@
+terraform {
+  backend "oss" {}
+  required_providers {
+    alicloud = {
+      source  = "aliyun/alicloud"
+      version = "1.247.0"
+    }
+  }
+}
+
 resource "null_resource" "dummy" {
   triggers = {
     timestamp = timestamp()
