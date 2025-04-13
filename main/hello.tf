ce8e244181d412a48b6a924a58fcc9c7f76353ed
kelvinxuande <37198547+kelvinxuande@users.noreply.github.com>
2025-04-13 10:27:04 +0800

    Update hello.tf

--- main/hello.tf
+++ main/hello.tf
@@ -1,5 +1,5 @@
-resource "null_resource" "default" {
-  provisioner "local-exec" {
-    command = "echo 'Hello World'"
+resource "null_resource" "dummy" {
+  triggers = {
+    timestamp = timestamp()
   }
 }
